75beaf61f03e6626b770da49d7870b09394c1eb3
fzzf0618 <fzzf0618@126.com>
2017-06-29 00:00:59 +0800

    capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,19 @@
+# config valid only for current version of Capistrano
+lock "3.8.2"
+
+set :application, "pepsi"
+set :repo_url, "git@example.com:me/my_repo.git"
+set :deploy_to, "/var/www/pepsi"
+append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "node_modules"
+set :keep_releases, 5
+
+set :pty,             true
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
