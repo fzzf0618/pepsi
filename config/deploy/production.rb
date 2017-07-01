8c1f4708bcde0b2013bb110e6568fcca2816d899
fzzf0618 <fzzf0618@126.com>
2017-07-01 12:53:03 +0800

    fix port

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '39.108.59.8', port: 25000, user: 'deploy', roles: %w{web app db}
+server '39.108.59.8', user: 'deploy', roles: %w{web app db}, port: 25000
 set :stage, :production
 
 # role-based syntax
