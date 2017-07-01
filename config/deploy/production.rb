be6ed00dd5d9f292edfaf0c455d95fdb70798721
fzzf0618 <fzzf0618@126.com>
2017-07-01 13:41:44 +0800

    .

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,13 @@
 set :stage, :production
-server '39.108.59.8', user: 'deploy', roles: %w{web app db}, primary: true
+server '39.108.59.8:25000', user: 'deploy', roles: %w{web app db}, primary: true
 set :application, "pepsi"
 set :repo_url, "git@github.com:fzzf0618/pepsi.git"
 set :branch, 'master'
 set :deploy_to, "/var/www/pepsi"
 set :user, "deploy"
 set :use_sudo, true
- set :ssh_options, {
-   user: 'deploy',
-   forward_agent: false,
-   port: 25000
- }
+set :ssh_options, {
+ forward_agent: false,
+ user: 'deploy',
+ port: 25000
+}
